f7f6aadb2f71750378c9b6e3df2dadb35d6cb366
krgirard33 <ken.girard@gmail.com>
2023-02-10 11:12:23 -0600

    Divide throws exception on 0, filled out CheckFileExtension, added ExtractFileName

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -2,30 +2,65 @@ package org.launchcode.exercises;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.lang.ArithmeticException;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        Divide(6, 2);
+        Divide(8,0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
-        studentFiles.put("Elizabeth", "MyCode.java");
+        studentFiles.put("Elizabeth", "MyCode.jav");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        ExtractFileName(studentFiles);
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        try {
+            Double answer = (double) (x / y);
+            System.out.println(answer);
+        } catch (ArithmeticException e){
+            System.out.println("You can not divide by 0.");
+
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        String lastFive = "";
+        String expected = ".java";
+
+         if (fileName.length()>5) {
+             lastFive = fileName.substring(fileName.length() - 5);
+             if (lastFive.equalsIgnoreCase(expected)) {
+                 return 1;
+             } else {
+                 return 0;
+             }
+         } else {
+             if (fileName == null || fileName.equals("")) {
+                 return -1;
+             } else {
+                 return 0;
+             }
+         }
+    }
+
+    private static void ExtractFileName(HashMap<String, String> students) {
+        //String fileName = null;
+        for (Map.Entry<String, String> student : students.entrySet()) {
+            String fileName = student.getValue();
+            String studentName = student.getKey();
+
+            System.out.println("Name: "+studentName+", Score: "+CheckFileExtension(fileName));
+        }
     }
 
 }
